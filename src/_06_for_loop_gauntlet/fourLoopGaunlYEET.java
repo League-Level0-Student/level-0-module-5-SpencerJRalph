3b3a373cceda89d4485734136524f9b3b65ff955
League Student <league@iMac-10.attlocal.net>
2019-12-09 18:27:59 -0800

    Update fourLoopGaunlYEET.java
    :bangbang:

--- src/_06_for_loop_gauntlet/fourLoopGaunlYEET.java
+++ src/_06_for_loop_gauntlet/fourLoopGaunlYEET.java
@@ -2,8 +2,44 @@ package _06_for_loop_gauntlet;
 
 public class fourLoopGaunlYEET {
 public static void main(String[] args) {
-	for (int i = 0; i > 101; i++) {
-		System.out.println(i);
+//for (int i = 0; i < 101; i++) {
+//	System.out.println(i);
+//}
+	
+
+//	for (int i = 101; i >=0 ; i--) {
+//		System.out.println(i);	}
+
+//	for (int i = 2; i < 101; i++) {
+//		 if (i%2==0) {
+//		System.out.println(i);
+//	}
+//	}
+ 
+//	for (int i = 1; i < 100; i++) {
+//		if (i%2==1) {
+//		System.out.println(i);
+//		}
+//	}
+	
+//	for (int i = 1; i < 501; i++) {
+//		if( i%2==1) {
+//			System.out.println(i + " is odd");
+//		}
+//		if( i%2==0) {
+//			System.out.println(i + " is even");
+//		}
+//	}
+	
+//	for (int i = 0; i < 777; i++) {
+//		if( i%7==0) {
+//			System.out.println(i);
+//		}
+//	}
+	int age = 14;
+	for (int i = 0; i < age;i++) {
+		int yearborn= 2019-age+i;
+		System.out.println("in"+ yearborn + " i was " + i + " years old.");
 	}
 }
 }
