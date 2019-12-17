20baf4320c46c19d9290048c3568eea44e45e3f8
League Student <league@iMac-15.attlocal.net>
2019-12-16 18:26:55 -0800

    Update fourLoopGaunlYEET.java
    IP :cake:

--- src/_06_for_loop_gauntlet/fourLoopGaunlYEET.java
+++ src/_06_for_loop_gauntlet/fourLoopGaunlYEET.java
@@ -1,11 +1,10 @@
 package _06_for_loop_gauntlet;
 
 public class fourLoopGaunlYEET {
-public static void main(String[] args) {
+	public static void main(String[] args) {
 //for (int i = 0; i < 101; i++) {
 //	System.out.println(i);
 //}
-	
 
 //	for (int i = 101; i >=0 ; i--) {
 //		System.out.println(i);	}
@@ -15,13 +14,13 @@ public static void main(String[] args) {
 //		System.out.println(i);
 //	}
 //	}
- 
+
 //	for (int i = 1; i < 100; i++) {
 //		if (i%2==1) {
 //		System.out.println(i);
 //		}
 //	}
-	
+
 //	for (int i = 1; i < 501; i++) {
 //		if( i%2==1) {
 //			System.out.println(i + " is odd");
@@ -30,16 +29,45 @@ public static void main(String[] args) {
 //			System.out.println(i + " is even");
 //		}
 //	}
-	
+
 //	for (int i = 0; i < 777; i++) {
 //		if( i%7==0) {
 //			System.out.println(i);
 //		}
 //	}
-	int age = 14;
-	for (int i = 0; i < age;i++) {
-		int yearborn= 2019-age+i;
-		System.out.println("in"+ yearborn + " i was " + i + " years old.");
+
+//	int age = 14;
+//	for (int i = 0; i < age;i++) {
+//		int yearborn= 2019-age+i;
+//		System.out.println("in"+ yearborn + " i was " + i + " years old.");
+//	}
+
+//	for (int i = 1; i < 4; i++) {
+//		for (int j = 0; j < 3; j++) {
+//			System.out.print(i);
+//			System.out.println(" "+ j);
+//		}
+//	}
+//		for (int i = 0; i < 3; i++) {
+//
+//			for (int j = 1; j < 4; j++) {
+//
+//				System.out.print(j + (i * 3));
+//			}
+//			System.out.println();
+//		}
+		
+//		for (int i = 0; i < 10; i++) {
+//			for (int j = 1; j < 11; j++) {
+//				System.out.print(j + (i *10));
+//			}
+//			System.out.println();
+//		}
+		
+		for (int i = 0; i < 6; i++) {
+			for (int j = 0; j < i; j++) {
+				System.out.println("*");
+			}
+		}
 	}
-}
 }
