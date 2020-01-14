63bb6bb2254fe4ce9984f1695a944b0e9eb90ba1
League Student <league@iMac-15.attlocal.net>
2020-01-13 18:25:46 -0800

    IP :panda_face:

--- src/_06_for_loop_gauntlet/fourLoopGaunlYEET.java
+++ src/_06_for_loop_gauntlet/fourLoopGaunlYEET.java
@@ -64,10 +64,13 @@ public class fourLoopGaunlYEET {
 //			System.out.println();
 //		}
 		
-		for (int i = 0; i < 6; i++) {
-			for (int j = 0; j < i; j++) {
-				System.out.println("*");
-			}
-		}
+//		for (int i = 0; i < 7; i++) {
+//		
+//			for (int j = 0; j < i; j++) {
+//				System.out.print("* ");
+//				
+//			}
+//			System.out.println();
+//		}
 	}
 }
